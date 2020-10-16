598cb55a4d5d29b77c16c8333631f175e0546bbd
idesoftwaredavis <idesoftwaredavis@gmail.com>
2020-10-16 04:43:54 -0300

    Layout

---
+++ Layout/js/materialize.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // navbar
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+    // parallax
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+
+    // carousel
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems,
+        {
+
+        });
+
+    var elems = document.querySelectorAll('.scrollspy');
+    var instances = M.ScrollSpy.init(elems);
+
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {
+      opacity:0.9,
+      inDuration: 600,
+      outDuration:300,
+
+    });
+});
