973a9afe391f95a6014da6dff44bb49d1ac6e2ed
idesoftwaredavis <idesoftwaredavis@gmail.com>
2020-10-23 02:02:38 -0300

    Template Admin

--- Layout/js/materialize.js
+++ Layout/js/materialize.js
@@ -32,6 +32,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, {});
+
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {});
 });
 
 
